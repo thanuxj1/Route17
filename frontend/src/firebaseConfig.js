c3338a7b98ce16054303dedc8fbc1d9152778d58
thanuxj1 <thxnujay@gmail.com>
2025-08-06 19:58:37 +0530

    api key securing

--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth"
 import { getAnalytics } from "firebase/analytics"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8ISut6oqyVPYjguaIwnPU8tRfLSGNkpQ",
-  authDomain: "route-17.firebaseapp.com",
-  projectId: "route-17",
-  storageBucket: "route-17.firebasestorage.app",
-  messagingSenderId: "245715303654",
-  appId: "1:245715303654:web:6ccb82739204d94ba70f27",
-  measurementId: "G-76M3R35DVG"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 }
 
 const app = initializeApp(firebaseConfig)
