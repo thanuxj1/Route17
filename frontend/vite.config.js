96478c32ac553d6f9fc3e0d34de97412fec03e7b
thanuxj1 <thxnujay@gmail.com>
2025-08-13 16:14:42 +0530

    vite config changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,51 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import compression from 'vite-plugin-compression';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const plugins = [react(), compression({ algorithm: 'brotliCompress' })];
-
-  if (mode === 'development') {
-    const { visualizer } = require('rollup-plugin-visualizer');
-    plugins.push(visualizer({ open: true, gzipSize: true }));
-  }
-
-  return {
-    plugins,
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            react: ['react', 'react-dom', 'react-router-dom'],
-            auth: ['@auth0/auth0-react'],
-            icons: ['lucide-react'],
-            charts: ['recharts'],
-            vendor: ['axios', 'date-fns']
-          }
-        }
-      },
-      chunkSizeWarningLimit: 1000
-    },
-    server: {
-      proxy: {
-        '/api': {
-          target: 'https://route17-production.up.railway.app',
-          changeOrigin: true,
-          rewrite: path => path.replace(/^\/api/, ''),
-          secure: false
-        }
-      }
-    },
-    css: {
-      modules: {
-        localsConvention: 'camelCase'
-      },
-      preprocessorOptions: {
-        scss: {
-          additionalData: `@import "./src/styles/variables.scss";`
-        }
-      }
-    }
-  };
-});
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
