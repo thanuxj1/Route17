883e520fde3087b8083355ee33a399cc8602057b
thanuxj1 <thxnujay@gmail.com>
2025-08-13 16:13:32 +0530

    vite config change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,46 +1,51 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
 import compression from 'vite-plugin-compression';
 
-export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({ open: true, gzipSize: true }),
-    compression({ algorithm: 'brotliCompress' })
-  ],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom', 'react-router-dom'],
-          auth: ['@auth0/auth0-react'],
-          icons: ['lucide-react'],
-          charts: ['recharts'],
-          vendor: ['axios', 'date-fns']
+export default defineConfig(({ mode }) => {
+  const plugins = [react(), compression({ algorithm: 'brotliCompress' })];
+
+  if (mode === 'development') {
+    const { visualizer } = require('rollup-plugin-visualizer');
+    plugins.push(visualizer({ open: true, gzipSize: true }));
+  }
+
+  return {
+    plugins,
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            react: ['react', 'react-dom', 'react-router-dom'],
+            auth: ['@auth0/auth0-react'],
+            icons: ['lucide-react'],
+            charts: ['recharts'],
+            vendor: ['axios', 'date-fns']
+          }
+        }
+      },
+      chunkSizeWarningLimit: 1000
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: 'https://route17-production.up.railway.app',
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, ''),
+          secure: false
         }
       }
     },
-    chunkSizeWarningLimit: 1000
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://route17-production.up.railway.app',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
-        secure: false
+    css: {
+      modules: {
+        localsConvention: 'camelCase'
+      },
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "./src/styles/variables.scss";`
+        }
       }
     }
-  },
-  css: {
-    modules: {
-      localsConvention: 'camelCase'
-    },
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./src/styles/variables.scss";`
-      }
-    }
-  }
+  };
 });
