3c1d8c29e9b4c7775b2f4497a9e0b70d6611345d
thanuxj1 <thxnujay@gmail.com>
2025-10-07 21:40:32 +0530

    comments not showing fixed

--- frontend/api/commentApi.js
+++ frontend/api/commentApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE = import.meta.env.VITE_API_BASE || "http://127.0.0.1:8000";
-const COMMENTS_URL = `${API_BASE}/comments/`;
+const COMMENTS_URL = `${API_BASE}/comments`;
 
 export async function getComments(busId) {
   try {
